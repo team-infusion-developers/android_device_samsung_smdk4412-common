b623b1900e3554466d50dcbbf29d7e4a324ee28b
Dominggoes Isakh <drjisakh@gmail.com>
2017-11-26 12:41:54 +0100

    galaxys2-common: power: Switch to Lineage interface
    Change-Id: I76a0c1b0815f7d69bf0b64f5e6f29f351fe339c8

--- common.mk
+++ common.mk
@@ -51,13 +51,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.power@1.0-impl \
+    vendor.lineage.power@1.0-impl \
+    vendor.lineage.power@1.0-service \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.keymaster@3.0-impl \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.light@2.0-impl
-
 
 #Camera
 PRODUCT_PACKAGES += \
