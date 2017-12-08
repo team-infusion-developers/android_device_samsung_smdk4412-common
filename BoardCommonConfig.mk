5de4accb05bcd6c4759a3014b75f7fd439737e71
Dominggoes Isakh <drjisakh@gmail.com>
2017-12-08 22:01:24 +0100

    galaxys2-common: Don't build libhealthd.lineage
    Change-Id: I2af5d2893520ba73bbfdc814939cc140163326b0

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -162,6 +162,7 @@ BOARD_SEPOLICY_DIRS += device/samsung/smdk4412-common/selinux
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 BOARD_BATTERY_DEVICE_NAME := "battery"
 BOARD_CHARGER_ENABLE_SUSPEND := true
+WITH_LINEAGE_CHARGER := false
 RED_LED_PATH := /sys/class/leds/led_r/brightness
 GREEN_LED_PATH := /sys/class/leds/led_g/brightness
 BLUE_LED_PATH := /sys/class/leds/led_b/brightness
