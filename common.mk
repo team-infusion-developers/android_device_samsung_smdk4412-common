611807e82af71bc3a78e74a1b7d81007928256ad
Dominggoes Isakh <drjisakh@gmail.com>
2018-05-13 21:42:27 +0200

    galaxys2-common: gps: Move gps.config to overlay
    Change-Id: I2e2a1f037b09ed32f79ccb08f310d2e587836a18

--- common.mk
+++ common.mk
@@ -43,6 +43,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl.exynos4 \
     gps.exynos4
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/gps_debug.conf:system/etc/gps_debug.conf \
+    $(COMMON_PATH)/configs/sirfgps.conf:system/etc/sirfgps.conf
+
 # Wifi
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
