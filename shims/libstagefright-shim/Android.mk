24a3727e59d05758eeadb637daaccfb6ca0718ae
Dominggoes Isakh <drjisakh@gmail.com>
2018-11-09 20:42:00 +0100

    galaxys2-common: libstagefright-shim: Remove SoftwareRenderer
    Must be updated for Pie.
    
    Change-Id: I8a99b5be944f1f2c9f426038ba0398d3600217a5

--- shims/libstagefright-shim/Android.mk
+++ shims/libstagefright-shim/Android.mk
@@ -16,8 +16,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := CameraSource.cpp \
-    colorconversion/SoftwareRenderer.cpp
+LOCAL_SRC_FILES := CameraSource.cpp
 
 LOCAL_C_INCLUDES := \
     $(TOP)/frameworks/av/include \
