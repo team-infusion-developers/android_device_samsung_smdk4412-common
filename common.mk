17fa5c61f3c3651ebdc0f21f85e7bc9c03b1eb35
Shilin Victor <chrono.monochrome@gmail.com>
2018-12-02 15:29:00 +0300

    smdk4412-common: fix derp
    Change-Id: Iade72bbd84d8ad881a00683f43142799fbdd8b73

--- common.mk
+++ common.mk
@@ -75,6 +75,7 @@ PRODUCT_PACKAGES := \
     libbt-vendor \
     com.android.future.usb.accessory \
     gralloc.exynos4 \
+    hwcomposer.exynos4 \
     android.hardware.camera.provider@2.4-impl-legacy \
     camera.device@1.0-impl-legacy \
     android.hardware.sensors@1.0-impl \
