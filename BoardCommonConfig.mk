1e916e9dbff427449b1b5244ad4dc2fd77081fec
Shilin Victor <chrono.monochrome@gmail.com>
2019-02-02 18:00:01 +0300

    smdk4412-common: add neverallows exemptions
    Change-Id: Ifc277591d01ac4cf9f544697c901f6f67acfd2e4

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -179,6 +179,8 @@ GREEN_LED_PATH := /sys/class/leds/led_g/brightness
 BLUE_LED_PATH := /sys/class/leds/led_b/brightness
 BACKLIGHT_PATH := /sys/class/backlight/panel/brightness
 
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Override healthd HAL
 BOARD_HAL_STATIC_LIBRARIES := libhealthd.exynos4
 
