6cfe28ab3e4a6eec3d3f44b6e7a1069635da7325
Shilin Victor <chrono.monochrome@gmail.com>
2019-02-03 23:31:43 +0300

    smdk4412-common: build missing Vibrator HIDL
    Change-Id: Ibe372f056917a1992e3701a2a1f75dea226930b3

--- common.mk
+++ common.mk
@@ -72,6 +72,7 @@ PRODUCT_PACKAGES := \
     android.hardware.keymaster@3.0-impl \
     android.hardware.light@2.0-impl \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.vibrator@1.0-impl \
     libbt-vendor \
     com.android.future.usb.accessory \
     hwcomposer.exynos4 \
