ec111975211910925c303d76c3123cc2a0f6fa80
Dominggoes Isakh <drjisakh@gmail.com>
2019-02-07 22:36:39 +0100

    galaxys2-common: Migrate to our Exynos4 LiveDisplay HIDL 2.0
    Change-Id: Ibd90f7595ff6ee2fa8dded3810c384cc79c6816c

--- common.mk
+++ common.mk
@@ -64,7 +64,7 @@ PRODUCT_COPY_FILES += \
 # Packages
 PRODUCT_PACKAGES += \
     AdvancedDisplay \
-    vendor.lineage.livedisplay@1.0-service-exynos4 \
+    vendor.lineage.livedisplay@2.0-service.samsung \
     android.hardware.graphics.allocator@2.0-impl-exynos4 \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
