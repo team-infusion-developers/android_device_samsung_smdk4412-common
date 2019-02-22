608618f3f657a59d6bd90ba0dcd0ab81b6f224c9
Shilin Victor <chrono.monochrome@gmail.com>
2019-02-22 13:28:19 +0300

    smdk4412-common: libsamsung_symbols is no longer needed
    requires https://github.com/CustomROMs/android_vendor_samsung_i9300/commit/438bc5d90d220a100e9da12b85c9079b8345b17a
    
    Change-Id: I5192ea9f3a658111a8914e85d11ec3d0879b4249

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -62,9 +62,7 @@ TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
 TARGET_LD_SHIM_LIBS := \
     /system/lib/libsuspend.so|libsuspend-shim.so \
-    /system/lib/libandroid_servers.so|libsuspend-shim.so \
-    /system/lib/libsec-ril.so|libsamsung_symbols.so \
-    /system/vendor/bin/hw/rild|libsamsung_symbols.so
+    /system/lib/libandroid_servers.so|libsuspend-shim.so
 
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
