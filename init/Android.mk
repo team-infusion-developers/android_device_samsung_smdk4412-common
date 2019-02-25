58335533759b0ecda086b5c2e333673f63ce7d7b
Dominggoes Isakh <drjisakh@gmail.com>
2019-02-25 22:03:56 +0100

    galaxys2-common: Add vendor init extension
    Change-Id: Ibfd090757c253b9cb0c3205ccdfbdc9aa42bb919

---
+++ init/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
+
+LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
+LOCAL_SRC_FILES := init_smdk4412-common.cpp
+LOCAL_MODULE := libinit_smdk4412-common
+
+include $(BUILD_STATIC_LIBRARY)
