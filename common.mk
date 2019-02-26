895026fec4d32cac908cf0fef4033922b1315d9b
Shilin Victor <chrono.monochrome@gmail.com>
2019-02-26 21:09:56 +0300

    Revert "galaxys2-common: Migrate to our Exynos4 LiveDisplay HIDL 2.0"
    This reverts commit ec111975211910925c303d76c3123cc2a0f6fa80.
    
    Change-Id: If41e460b9e311300b04c0daebf534d0c76e7d6fc

--- common.mk
+++ common.mk
@@ -76,7 +76,7 @@ PRODUCT_COPY_FILES += \
 # Packages
 PRODUCT_PACKAGES += \
     AdvancedDisplay \
-    vendor.lineage.livedisplay@2.0-service.samsung \
+    vendor.lineage.livedisplay@1.0-service-exynos4 \
     android.hardware.graphics.allocator@2.0-impl-exynos4 \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
