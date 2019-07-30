LineageOS/android_device_samsung_galaxys2-common

Added Bluetooth SIM Access Profile support to shim library and Bluetooth.apk
Change-Id: Ibe597d18324dc0aba784ec52f147d9d81da277de

shims/libsecril-shim/Android.mk
@@ -2,8 +2,19 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES:= secril-shim.c
+LOCAL_SRC_FILES:= \
+	proto/pb_common.c \
+	proto/pb_decode.c \
+	proto/pb_encode.c \
+	proto/sap-api.pb.c \
+	sap-functions.c \
+    secril-sap.c \
+    secril-shim.c \
+    sec-sap.c
+
 LOCAL_SHARED_LIBRARIES := liblog libbinder
 LOCAL_MODULE:= libsecril-shim
+LOCAL_C_INCLUDES += proto
+LOCAL_CFLAGS += -DPB_ENABLE_MALLOC
 
 include $(BUILD_SHARED_LIBRARY)

team-infusion-developers/android_device_samsung_smdk4412-common

shims/libsecril-shim/Android.mk
@@ -4,6 +4,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
+	proto/pb_common.cpp \
+	proto/pb_decode.cpp \
+	proto/pb_encode.cpp \
+	proto/sap-api.pb.cpp \
+	sap-functions.cpp \
+	secril-sap.cpp \
+	sec-sap.cpp \
 	secril-shim.cpp
 
 LOCAL_SHARED_LIBRARIES := \
@@ -12,7 +19,8 @@ LOCAL_SHARED_LIBRARIES := \
 	libcutils \
 	libbinder
 
-LOCAL_CFLAGS := -Wall -Werror
+LOCAL_C_INCLUDES += proto
+LOCAL_CFLAGS := -Wall -Werror -DPB_ENABLE_MALLOC -Wno-unused-parameter
 
 LOCAL_MODULE := libsecril-shim
 